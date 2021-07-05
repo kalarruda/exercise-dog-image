115288c61303d05303aa8ef8f1f4fdaa2a4c2823
José Carlos <kalarruda@hotmail.com>
2021-07-05 11:31:23 -0300

    dog-exercises

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: "",
+    }
+
+    this.fetchDog = this.fetchDog.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchDog();
+  }
+
+  fetchDog() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(result => this.setState({ data: result }));
+  }
+  render() {
+    if (this.data === "") return "loading...";
+      
+      return (
+        <div>
+          <p>Doguinhos</p>
+          <button onClick={this.fetchDog}>Novo doguinho</button>
+          <div>
+            <img src={ this.state.data.message } alt="Radom dog"/>
+          </div> 
+        </div>
+    )
+  }
 }
 
 export default App;
